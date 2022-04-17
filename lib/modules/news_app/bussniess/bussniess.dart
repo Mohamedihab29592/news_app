09fcea5e3c3616bb639756429f68840ebdf85ae0
Mohamedihab29592 <mohamed_ihab.92@hotmail.com>
2022-04-17 13:29:20 +0200

    1st

---
+++ lib/modules/news_app/bussniess/bussniess.dart
@@ -0,0 +1,31 @@
+// ignore: unused_import
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../layout/newsapp/cubit/cubit.dart';
+import '../../../layout/newsapp/cubit/statue.dart';
+import '../../../shared/components/components.dart';
+
+
+class BusinessScreen extends StatelessWidget {
+  const BusinessScreen({Key? key}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context)=>NewsCubit()..getBusiness()..getSports()..getScience(),
+      child: BlocConsumer<NewsCubit,NewsStates>(
+        listener: (context,statue){},
+        builder: (context,state){
+          var list = NewsCubit.get(context).business;
+          return articleBuilder(list,context);
+        },
+      ),
+    );
+
+    }
+}
